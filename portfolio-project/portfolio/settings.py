70127ac8167145e7a3e66d0fe5ce55db96866dea
Ben Thornhill <bthornhill123@gmail.com>
2021-10-05 19:48:52 -0600

    Set up postgres database with Job model

--- portfolio-project/portfolio/settings.py
+++ portfolio-project/portfolio/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
